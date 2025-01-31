dba80f483222c3359d80950ecf5023ec9c2a32f9
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2025-01-30 22:53:12 -0300

    feat: integrated redirect to user creation in navbar

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,12 +1,22 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { AvatarComponent } from '../avatar/avatar.component';
+import { ButtonComponent } from '../button/button.component';
+import { ButtonFabDirective } from '../button/directives/button-fab.directive';
 
 @Component({
   selector: 'app-navbar',
-  imports: [AvatarComponent],
+  imports: [AvatarComponent, ButtonComponent, ButtonFabDirective],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   @Input() public userIsLogged: boolean = false;
+
+  constructor(private router: Router) {}
+
+  public redirectToUserCreation(): void {
+    this.router.navigateByUrl('/user/register');
+  }
 }
