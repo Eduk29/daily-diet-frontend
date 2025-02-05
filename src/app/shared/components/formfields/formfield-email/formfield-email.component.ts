eac8a51043d61a275a65bb9a954e9dbba0907338
José Eduardo Trindade E Marques <edu.temarques@gmail.com>
2025-02-04 21:54:43 -0300

    feat: impl. generic email form field component

---
+++ src/app/shared/components/formfields/formfield-email/formfield-email.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormfieldLabelPositionType } from '../types/formfield-label-position.type';
+
+@Component({
+  selector: 'app-formfield-email',
+  imports: [ReactiveFormsModule],
+  templateUrl: './formfield-email.component.html',
+  styleUrl: './formfield-email.component.scss',
+})
+export class FormfieldEmailComponent implements OnInit {
+  @Input() customClass: string = '';
+  @Input() controlName!: FormControl<unknown>;
+  @Input() hint!: string;
+  @Input() label!: string;
+  @Input() labelPosition: FormfieldLabelPositionType = 'before';
+  @Input() placeholder: string = '';
+
+  ngOnInit(): void {
+    if (!this.controlName) {
+      console.warn('FormfieldTextComponent: controlName is required');
+    }
+
+    if (!this.label) {
+      console.warn('FormfieldTextComponent: label is required');
+    }
+  }
+
+  public get labelFor(): string {
+    return `${this.label}_input`;
+  }
+
+  public get labelPositionClass(): string {
+    return this.labelPosition === 'before' ? 'label-before' : 'label-above';
+  }
+}
